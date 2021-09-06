2dc470e6b6c93d43ed626fbd186f95f5410ed53c
Karl Galagar <fgalagar@brierley.com>
2021-09-07 02:08:09 +0800

    delete task from api

--- src/App.js
+++ src/App.js
@@ -40,7 +40,8 @@ function App() {
     setShowAddTask(state);
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async(id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'});
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
